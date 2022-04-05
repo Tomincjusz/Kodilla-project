39bb0ca89c14596958f69a0a56ca4c01d3edc4f0
Tomasz Gawe <tomincjusz@gmail.com>
2022-04-05 18:30:00 +0200

    kodilla 17.6

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -40,14 +40,14 @@ public class TaskController {
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping(value = "/tasks")
+    @PutMapping
     public ResponseEntity<TaskDto> updateTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         Task savedTask = service.saveTask(task);
         return ResponseEntity.ok(taskMapper.mapToTaskDto(savedTask));
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,value = "/tasks")
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         service.saveTask(task);
